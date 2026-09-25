59fc026a2cac19184424326f2b02de9f17f5684f
Matheus Campelo Teles <campelomatheusteles@gmail.com>
2026-09-25 14:15:22 -0300

    Cria funcao calcular valor base

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+float calcular_valor_base(float distancia) {
+    if (distancia <= 5.00) {
+        return 8.00;
+    }
+    if (distancia <= 15.00) {
+        return 12.00;
+    }
+    if (distancia <= 30.00) {
+        return 18.00;
+    }
+    return 0.25;
+}
+
 int main() {
-  return 0;
+    return 0;
 }
